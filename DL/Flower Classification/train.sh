50141d9953386cc08e58be0fdb62c18c8292ea69
huynth1801 <huuhuy1801@gmail.com>
2022-01-29 19:41:38 +0700

    add shell script

--- DL/Flower Classification/train.sh
+++ DL/Flower Classification/train.sh
@@ -1,5 +1,5 @@
-python cli.py \
-    --trainer.gpus=0         \
+!python cli.py fit \
+    --trainer.gpus=1         \
     --trainer.max_epochs=40 \
     --trainer.default_root_dir='logs/mlp_mixer' \
     --trainer.log_every_n_steps=5 \
@@ -7,7 +7,7 @@ python cli.py \
     --model.optimizer="adam" \
     --model.learning_rate=0.001 \
     --model.num_classes=5 \
-    --data.data_dir="/home/ubuntu/splitted_flowers" \
+    --data.data_dir="/content/Pytorch_practice/DL/Flower Classification/data/flower_photos" \
     --data.batch_size=32 \
     --wandb_project_name="Flower Classification" \
     --wandb_task_name="MLP Mixer" \
