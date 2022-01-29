4483b72c62014cb1ff47b2e35f1ed46c743adbe2
huynth1801 <huuhuy1801@gmail.com>
2022-01-29 14:50:16 +0700

    add shell script

---
+++ DL/Flower Classification/get_data.sh
@@ -0,0 +1,1 @@
+wget https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz && tar xzf flower_photos.tgz
---
+++ DL/Flower Classification/train.sh
@@ -0,0 +1,14 @@
+python cli.py \
+    --trainer.gpus=0         \
+    --trainer.max_epochs=40 \
+    --trainer.default_root_dir='logs/vanila_mlp' \
+    --trainer.log_every_n_steps=5 \
+    --model.model_name="vanila" \
+    --model.optimizer="adam" \
+    --model.learning_rate=0.001 \
+    --model.num_classes=5 \
+    --data.data_dir="/home/ubuntu/splitted_flowers" \
+    --data.batch_size=32 \
+    --wandb_project_name="Flower Classification" \
+    --wandb_task_name="MLP Mixer" \
+    --use_wandb
